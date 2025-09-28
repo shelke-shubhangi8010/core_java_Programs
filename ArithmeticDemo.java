3e3be2c39ae7c1e9295227729dab34125f43a326
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2025-09-28 21:34:06 +0530

    Create ArithmeticDemo.java

---
+++ ArithmeticDemo.java
@@ -0,0 +1,13 @@
+class BitwiseDemo {
+    public static void main(String args[]) {
+        int no1 = 10;
+        int no2 = 9;
+
+        System.out.println("Bitwise And no1&no2" + (no1 & no2));
+        System.out.println("Bitwise OR  no1|no2" + (no1 | no2));
+        System.out.println("Bitwise XOR  no1^no2" + (no1 ^ no2));
+
+        System.out.println("Bitwise Complement of no1=" + ~(no1));
+
+    }
+}
